f926281bb0ef51c18c124c2cbac8ef637d2af9db
Haridas250503 <haridasshinde314@gmail.com>
2026-02-28 22:53:10 +0530

    adding new module for practice

---
+++ interviewer-module/src/main/java/com/demo/interviewer_module/InterviewerModuleApplication.java
@@ -0,0 +1,13 @@
+package com.demo.interviewer_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerModuleApplication.class, args);
+	}
+
+}
