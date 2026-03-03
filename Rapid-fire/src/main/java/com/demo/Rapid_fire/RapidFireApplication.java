1628ae804a51497f29772cc91410ff6d0a628fed
Haridas250503 <haridasshinde314@gmail.com>
2026-03-03 23:33:52 +0530

    "initial commit of project"

---
+++ Rapid-fire/src/main/java/com/demo/Rapid_fire/RapidFireApplication.java
@@ -0,0 +1,13 @@
+package com.demo.Rapid_fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidFireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidFireApplication.class, args);
+	}
+
+}
---
+++ Rapid-fire/src/test/java/com/demo/Rapid_fire/RapidFireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.Rapid_fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
