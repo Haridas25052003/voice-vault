c3b79fb7887d46d7aab01513e8796d8444ae3458
Haridas Shinde <haridasshinde314@gmail.com>
2026-04-07 01:20:27 +0530

    new initial app

---
+++ akash/src/test/java/Image_generator/akash/AkashApplicationTests.java
@@ -0,0 +1,13 @@
+package Image_generator.akash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
